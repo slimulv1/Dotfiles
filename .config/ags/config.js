629aae9fced9d4d3b3ad76c5a09443eddfbc5582
slimulv1 <slimulv1@naver.com>
2023-12-28 10:17:15 +0700

    fix battery

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,7 +1,6 @@
 "strict mode";
 // Import
 import { App, Utils } from './imports.js';
-import { firstRunWelcome } from './services/messages.js';
 // Widgets
 import Bar from './widgets/bar/main.js';
 import Cheatsheet from './widgets/cheatsheet/main.js';
@@ -15,18 +14,20 @@ import Session from './widgets/session/main.js';
 import SideLeft from './widgets/sideleft/main.js';
 import SideRight from './widgets/sideright/main.js';
 
-// Longer than actual anim time (see styles) to make sure widgets animate fully
-const CLOSE_ANIM_TIME = 210;
+const CLOSE_ANIM_TIME = 210; // Longer than actual anim time (see styles) to make sure widgets animate fully
 
 // Init cache and check first run
 Utils.exec(`bash -c 'mkdir -p ~/.cache/ags/user/colorschemes'`);
-
 // SCSS compilation
 Utils.exec(`bash -c 'echo "" > ${App.configDir}/scss/_musicwal.scss'`); // reset music styles
 Utils.exec(`bash -c 'echo "" > ${App.configDir}/scss/_musicmaterial.scss'`); // reset music styles
-Utils.exec(`sassc ${App.configDir}/scss/main.scss ${App.configDir}/style.css`);
-App.resetCss();
-App.applyCss(`${App.configDir}/style.css`);
+function applyStyle() {
+    Utils.exec(`sassc ${App.configDir}/scss/main.scss ${App.configDir}/style.css`);
+    App.resetCss();
+    App.applyCss(`${App.configDir}/style.css`);
+    console.log('[LOG] Styles loaded')
+}
+applyStyle();
 
 // Config object
 export default {
@@ -52,13 +53,6 @@ export default {
         SideRight(),
         Osk(), // On-screen keyboard
         Session(), // Power menu, if that's what you like to call it
+        Bar()
     ],
 };
-
-// We don't want context menus of the bar's tray go under the rounded corner below,
-// so bar is returned after default export, making it get spawned after the corner
-// (having an Utils.timeout in that window array just gives an error)
-// Not having it in default export is fine since we don't need to toggle it
-Bar(); 
-
-// uwu 
