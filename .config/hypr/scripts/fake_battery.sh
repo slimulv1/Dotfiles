91c8def4169c787554cfb706e85f33ab10333966
slimulv1 <slimulv1@naver.com>
2024-02-20 22:55:20 +0700

    adopt hypridle and hyprlock; byebye swayidle and swaylock ᗜˬᗜ

--- .config/hypr/scripts/fake_battery.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-me=$(whoami)
-cd /home/$me/linux-fake-battery-module/
-echo My7skjawwaqert | sudo -S insmod ./fake_battery.ko
-echo 'charging = 1' | sudo tee /dev/fake_battery
-echo 'capacity0 = 88' | sudo tee /dev/fake_battery
-echo 'capacity1 = 88' | sudo tee /dev/fake_battery
-kill -9 $PPID
--- .config/hypr/scripts/update-betterdiscord.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cd ~/BetterDiscord && pnpm inject
