db638db18bbda2c89c4afe9b691d1550c94b1270
slimulv1 <slimulv1@naver.com>
2024-04-05 11:12:11 +0700

    up

---
+++ update-hyprland.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# This script showed one reason about why it's good to split functions and installers in another file.
+#
+# This script is for install/update ags itself, NOT the config for it.
+# The install.sh will install ags for you, why you still need this?
+# Because you may run this script to ONLY update ags, since ags is a very active project currently and updates frequently.
+#
+cd Downloads/
+git clone --recursive https://github.com/hyprwm/Hyprland
+cd Hyprland
+make all && sudo make install
